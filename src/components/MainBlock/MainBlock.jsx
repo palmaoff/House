fc60d896a513cde56b2df2eb157bd93237c5a9bf
palmaoff <fedorin.anton@mail.ru>
2020-07-21 19:14:28 +0300

    adaptive text mainblock

--- src/components/MainBlock/MainBlock.jsx
+++ src/components/MainBlock/MainBlock.jsx
@@ -12,14 +12,20 @@ const MainBlock = () => {
 						Управляющая Компания "Полёт"
 					</div>
 					<div className={css.body}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-						sed do eiusmod tempor incididunt ut labore et dolore magna
-						aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-						ullamco laboris nisi ut aliquip ex ea commodo consequat.
-						Duis aute irure dolor in reprehenderit in voluptate velit
-						esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-						occaecat cupidatat non proident, sunt in culpa qui officia
-						deserunt mollit anim id est laborum.
+						<div className={css.part}>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+							sed do eiusmod tempor incididunt ut labore et dolore magna
+							aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+						</div>
+						<div className={css.part}>
+							ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Duis aute irure dolor in reprehenderit in voluptate velit
+							esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+						</div>
+						<div className={css.part}>
+							occaecat cupidatat non proident, sunt in culpa qui officia
+							deserunt mollit anim id est laborum.
+						</div>
 					</div>
 				</div>
 			</div>
