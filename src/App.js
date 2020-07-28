0fe2722d2260e8a38796921a85610c2eb30d7d6c
palmaoff <fedorin.anton@mail.ru>
2020-07-28 13:24:02 +0300

    scroll to the top component & contacts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import MainBlock from './components/MainBlock/MainBlock'
 import Bottom from './components/Bottom/Bottom'
 import { BrowserRouter, Route } from 'react-router-dom'
 import HouseListContainer from './components/HouseList/HouseListContainer'
+import Contacts from './components/Contacts/Contacts'
+import ScrollToTheTop from './components/ScrollToTheTop'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
+      <ScrollToTheTop />
       <Header />
       <div className="field"></div>
-      <Route path='/main' component={MainBlock}/>
-      <Route path='/houses' component={HouseListContainer}/>
+      <Route path='/main' component={MainBlock} />
+      <Route path='/houses' component={HouseListContainer} />
+      <Route path='/contacts' component={Contacts} />
       <Bottom />
     </BrowserRouter>
   );
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from './Contacts.module.css'
+
+const Contacts = () => {
+	return (
+		<div className={css.wrapper}>
+			Contacts
+		</div>
+	)
+}
+
+export default Contacts
