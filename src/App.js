e1e7f076dd0ddfe6f93d697801fe862d050b8d1c
palmaoff <fedorin.anton@mail.ru>
2020-08-01 21:51:40 +0300

    contacts elements

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, { useEffect } from 'react'
+import React,{ useEffect } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import MainBlock from './components/MainBlock/MainBlock'
 import Bottom from './components/Bottom/Bottom'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter,Route } from 'react-router-dom'
 import HouseListContainer from './components/HouseList/HouseListContainer'
 import Contacts from './components/Contacts/Contacts'
 import ScrollToTheTop from './components/ScrollToTheTop'
 
 const App = () => {
   return (
-    <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
+    <BrowserRouter onUpdate={() => window.scrollTo(0,0)}>
       <ScrollToTheTop />
       <Header />
       <div className="field"></div>
-      <Route path='/main' component={MainBlock} />
+      <Route path='/main'
+        render={() =>
+          <>
+            <MainBlock />
+            <Contacts />
+          </>
+        } />
       <Route path='/houses' component={HouseListContainer} />
       <Route path='/contacts' component={Contacts} />
       <Bottom />
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,7 +4,35 @@ import css from './Contacts.module.css'
 const Contacts = () => {
 	return (
 		<div className={css.wrapper}>
-			Contacts
+			<div className={css.title}>
+				Наши Контакты:
+			</div>
+			<div className={css.persons}>
+				<div className={css.person}>
+					<div className={css.img}>
+						<img src="#"></img>
+					</div>
+					<div className={css.position}>
+						Директор:
+				</div>
+					<div className={css.name}>
+						Науменко Максим Александрович
+				</div>
+					<div className={css.phone}>8-915-540-33-99</div>
+				</div>
+				<div className={css.person}>
+					<div className={css.img}>
+						<img src="#"></img>
+					</div>
+					<div className={css.position}>
+						Заместитель:
+				</div>
+					<div className={css.name}>
+						Корсун Елена Александровна
+				</div>
+					<div className={css.phone}>8-915-540-33-99</div>
+				</div>
+			</div>
 		</div>
 	)
 }
