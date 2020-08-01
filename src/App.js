0837fec2f3134e79c0456100e784e605fb131371
palmaoff <fedorin.anton@mail.ru>
2020-08-01 19:00:57 +0300

    header find a right place

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import './App.css'
-import Header from './Header/Header'
+import Header from './components/Header/Header'
 import MainBlock from './components/MainBlock/MainBlock'
 import Bottom from './components/Bottom/Bottom'
 import { BrowserRouter, Route } from 'react-router-dom'
